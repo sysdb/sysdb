b71f47acb89bd69ab204e9b8f550a2690087dfdc
Sebastian Harl <sh@tokkee.org>
2014-02-22 20:14:26 -0800

    t/coverage.sh: Fall-back to lcov without --no-external.
    Old versions of lcov don't support that flag. Simply try to fall back in case
    the first run fails.

--- t/coverage.sh
+++ t/coverage.sh
@@ -46,8 +46,11 @@ make
 
 lcov --base-directory src --directory src --zerocount
 make test
+# old versions of lcov don't support --no-external
 lcov --base-directory src --directory src --no-external \
-	--capture -o sysdb_coverage.info
+	--capture -o sysdb_coverage.info \
+	|| lcov --base-directory src --directory src \
+		--capture -o sysdb_coverage.info
 
 V=$( ./version-gen.sh )
 genhtml -o "$srcdir"/t/coverage \
