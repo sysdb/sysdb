0751b423192f2d9f17590f9db7a074e23b5b8dec
Sebastian Harl <sh@tokkee.org>
2014-04-22 19:49:27 +0200

    sysdbd: Shut down all plugins before exiting.

--- src/tools/sysdbd/main.c
+++ src/tools/sysdbd/main.c
@@ -375,6 +375,7 @@ main(int argc, char **argv)
 
 	sdb_log(SDB_LOG_INFO, "Shutting down SysDB daemon "SDB_VERSION_STRING
 			SDB_VERSION_EXTRA" (pid %i)", (int)getpid());
+	sdb_plugin_shutdown_all();
 	return status;
 } /* main */
 
