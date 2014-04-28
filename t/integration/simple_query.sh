97aa30ed76096fbd663290d340de40a9ca7acdab
Sebastian Harl <sh@tokkee.org>
2014-04-28 08:00:30 +0200

    t/integration/simple_query: Added test doing a simple query.

---
+++ t/integration/simple_query.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+#
+# SysDB -- t/integration/simple_query.sh
+# Copyright (C) 2012 Sebastian 'tokkee' Harl <sh@tokkee.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#
+# Integration tests using simple queries.
+#
+
+set -e
+
+source "$( dirname "$0" )/test_lib.sh"
+
+cat <<EOF > "$TESTDIR/sysdbd.conf"
+Listen "$SOCKET_FILE"
+PluginDir "$PLUGIN_DIR"
+Interval 2
+
+LoadBackend mock_plugin
+<Backend "test::integration::mock">
+</Backend>
+EOF
+
+"$TOP_SRCDIR/src/sysdbd" -D -C "$TESTDIR/sysdbd.conf" &
+sysdbd_pid=$!
+
+wait_for_sysdbd
+sleep 3
+
+"$TOP_SRCDIR/src/sysdb" -H "$SOCKET_FILE" -c LIST \
+	| grep -F '"host1.example.com"' \
+	| grep -F "host2.example.com" \
+	| grep -F "localhost" \
+	| grep -F "other.host.name" \
+	| grep -F "some.host.name"
+kill $!
