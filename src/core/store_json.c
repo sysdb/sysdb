sysdb/sysdb

commit 3603db13105aefac4d6ceb356bcfd118a92fa654

store_json: Moved special casing of parent objects into the caller.
This makes the JSON module more generic by removing a special case of the
memory store. The caller may now emit arbitrary objects and the formatter will
do the right thing as long as parent-child relationships are right.

src/core/store_json.c
@@ -1,6 +1,6 @@
 /*
  * SysDB - src/core/store_json.c
- * Copyright (C) 2013-2014 Sebastian 'tokkee' Harl <sh@tokkee.org>
+ * Copyright (C) 2013-2015 Sebastian 'tokkee' Harl <sh@tokkee.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -58,9 +58,6 @@ struct sdb_store_json_formatter {
 	int context[8];
 	size_t current;
 
-	/* The current host context when processing non-host objects */
-	sdb_store_obj_t *current_host;
-
 	int type;
 	int flags;
 };
@@ -83,8 +80,6 @@ formatter_init(sdb_object_t *obj, va_list ap)
 
 	f->context[0] = 0;
 	f->current = 0;
-
-	f->current_host = NULL;
 	return 0;
 } /* formatter_init */
 
@@ -134,17 +129,6 @@ json_emit(sdb_store_json_formatter_t *f, sdb_store_obj_t *obj)
 	size_t i;
 
 	assert(f && obj);
-
-	if ((f->type != SDB_HOST) && (f->type == obj->type)) {
-		/* create the host for the current entry first */
-		assert(obj->parent && (obj->parent->type == SDB_HOST));
-		if (f->current_host != obj->parent) {
-			json_emit(f, obj->parent);
-			sdb_strbuf_append(f->buf, ", \"%ss\": [",
-					SDB_STORE_TYPE_TO_NAME(obj->type));
-			f->current_host = obj->parent;
-		}
-	}
 
 	escape_string(SDB_OBJ(obj)->name, name);
 	sdb_strbuf_append(f->buf, "{\"name\": %s, ", name);
@@ -211,16 +195,9 @@ sdb_store_json_emit(sdb_store_json_formatter_t *f, sdb_store_obj_t *obj)
 	if ((! f) || (! obj))
 		return -1;
 
-	if ((f->type != SDB_HOST) && (obj->type == SDB_HOST)) {
-		sdb_log(SDB_LOG_ERR, "store: Unexpected object of type host "
-				"during %s JSON serialization",
-				SDB_STORE_TYPE_TO_NAME(f->type));
-		return -1;
-	}
-
 	/* first top-level object */
 	if (! f->context[0]) {
-		if (obj->type != f->type) {
+		if ((obj->type != f->type) && (obj->type != SDB_HOST)) {
 			sdb_log(SDB_LOG_ERR, "store: Unexpected object of type %s "
 					"as the first element during %s JSON serialization",
 					SDB_STORE_TYPE_TO_NAME(obj->type),
@@ -230,42 +207,38 @@ sdb_store_json_emit(sdb_store_json_formatter_t *f, sdb_store_obj_t *obj)
 		if (f->flags & SDB_WANT_ARRAY)
 			sdb_strbuf_append(f->buf, "[");
 		assert(f->current == 0);
-		f->context[0] = obj->type;
-		return json_emit(f, obj);
-	}
-
-	if ((f->current >= 1) && (obj->type != SDB_ATTRIBUTE)) {
-		/* new entry of a previous type or a new type on the same level;
-		 * rewind to the right state */
-		while (f->current > 0) {
-			if (f->context[f->current] == obj->type)
-				break;
-			sdb_strbuf_append(f->buf, "}]");
-			--f->current;
-		}
-	}
-	if ((obj->type == f->type) && (f->type != SDB_HOST))
-		if (obj->parent != f->current_host)
-			sdb_strbuf_append(f->buf, "}]");
-
-	if (obj->type == f->context[f->current]) {
-		/* new entry of the same type */
-		sdb_strbuf_append(f->buf, "},");
-	}
-	else if ((f->context[f->current] == SDB_HOST)
-			|| (obj->type == SDB_ATTRIBUTE)) {
-		assert(obj->type != SDB_HOST);
-		/* all object types may be children of a host;
-		 * attributes may be children of any type */
-		sdb_strbuf_append(f->buf, ", \"%ss\": [",
-				SDB_STORE_TYPE_TO_NAME(obj->type));
-		++f->current;
 	}
 	else {
-		sdb_log(SDB_LOG_ERR, "store: Unexpected object of type %s "
-				"on level %zu during JSON serialization",
-				SDB_STORE_TYPE_TO_NAME(obj->type), f->current);
-		return -1;
+		if ((f->current >= 1) && (obj->type != SDB_ATTRIBUTE)) {
+			/* new entry of a previous type or a new type on the same level;
+			 * rewind to the right state */
+			while (f->current > 0) {
+				if (f->context[f->current] == obj->type)
+					break;
+				sdb_strbuf_append(f->buf, "}]");
+				--f->current;
+			}
+		}
+
+		if (obj->type == f->context[f->current]) {
+			/* new entry of the same type */
+			sdb_strbuf_append(f->buf, "},");
+		}
+		else if ((f->context[f->current] == SDB_HOST)
+				|| (obj->type == SDB_ATTRIBUTE)) {
+			assert(obj->type != SDB_HOST);
+			/* all object types may be children of a host;
+			 * attributes may be children of any type */
+			sdb_strbuf_append(f->buf, ", \"%ss\": [",
+					SDB_STORE_TYPE_TO_NAME(obj->type));
+			++f->current;
+		}
+		else {
+			sdb_log(SDB_LOG_ERR, "store: Unexpected object of type %s "
+					"on level %zu during JSON serialization",
+					SDB_STORE_TYPE_TO_NAME(obj->type), f->current);
+			return -1;
+		}
 	}
 
 	json_emit(f, obj);
@@ -339,12 +312,7 @@ sdb_store_json_finish(sdb_store_json_formatter_t *f)
 		sdb_strbuf_append(f->buf, "}]");
 		--f->current;
 	}
-	if (f->context[0] != SDB_HOST) {
-		assert(f->type != SDB_HOST);
-		sdb_strbuf_append(f->buf, "}]}");
-	}
-	else
-		sdb_strbuf_append(f->buf, "}");
+	sdb_strbuf_append(f->buf, "}");
 
 	if (f->flags & SDB_WANT_ARRAY)
 		sdb_strbuf_append(f->buf, "]");

src/core/store_exec.c
@@ -42,21 +42,39 @@
  * private helper functions
  */
 
+typedef struct {
+	sdb_store_json_formatter_t *f;
+	sdb_store_obj_t *current_host;
+} iter_t;
+
+static int
+maybe_emit_host(iter_t *iter, sdb_store_obj_t *obj)
+{
+	if ((obj->type == SDB_HOST) || (obj->type == SDB_ATTRIBUTE))
+		return 0;
+	if (iter->current_host == obj->parent)
+		return 0;
+	iter->current_host = obj->parent;
+	return sdb_store_json_emit(iter->f, obj->parent);
+} /* maybe_emit_host */
+
 static int
 list_tojson(sdb_store_obj_t *obj,
 		sdb_store_matcher_t __attribute__((unused)) *filter,
 		void *user_data)
 {
-	sdb_store_json_formatter_t *f = user_data;
-	return sdb_store_json_emit(f, obj);
+	iter_t *iter = user_data;
+	maybe_emit_host(iter, obj);
+	return sdb_store_json_emit(iter->f, obj);
 } /* list_tojson */
 
 static int
 lookup_tojson(sdb_store_obj_t *obj, sdb_store_matcher_t *filter,
 		void *user_data)
 {
-	sdb_store_json_formatter_t *f = user_data;
-	return sdb_store_json_emit_full(f, obj, filter);
+	iter_t *iter = user_data;
+	maybe_emit_host(iter, obj);
+	return sdb_store_json_emit_full(iter->f, obj, filter);
 } /* lookup_tojson */
 
 /*
@@ -74,6 +92,7 @@ exec_fetch(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 	sdb_store_obj_t *obj;
 
 	sdb_store_json_formatter_t *f;
+	int status = 0;
 
 	if ((! name) || ((type == SDB_HOST) && hostname)
 			|| ((type != SDB_HOST) && (! hostname))) {
@@ -127,7 +146,9 @@ exec_fetch(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 	}
 
 	sdb_strbuf_memcpy(buf, &res_type, sizeof(uint32_t));
-	if (sdb_store_json_emit_full(f, obj, filter)) {
+	if (type != SDB_HOST)
+		status = sdb_store_json_emit(f, obj->parent);
+	if (status || sdb_store_json_emit_full(f, obj, filter)) {
 		sdb_log(SDB_LOG_ERR, "frontend: Failed to serialize "
 				"%s %s.%s to JSON", SDB_STORE_TYPE_TO_NAME(type),
 				hostname, name);
@@ -149,10 +170,10 @@ exec_list(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 		int type, sdb_store_matcher_t *filter)
 {
 	uint32_t res_type = htonl(SDB_CONNECTION_LIST);
-	sdb_store_json_formatter_t *f;
+	iter_t iter = { NULL, NULL };
 
-	f = sdb_store_json_formatter(buf, type, SDB_WANT_ARRAY);
-	if (! f) {
+	iter.f = sdb_store_json_formatter(buf, type, SDB_WANT_ARRAY);
+	if (! iter.f) {
 		char err[1024];
 		sdb_log(SDB_LOG_ERR, "frontend: Failed to create "
 				"JSON formatter to handle LIST command: %s",
@@ -163,16 +184,16 @@ exec_list(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 	}
 
 	sdb_strbuf_memcpy(buf, &res_type, sizeof(uint32_t));
-	if (sdb_store_scan(store, type, /* m = */ NULL, filter, list_tojson, f)) {
+	if (sdb_store_scan(store, type, /* m = */ NULL, filter, list_tojson, &iter)) {
 		sdb_log(SDB_LOG_ERR, "frontend: Failed to serialize "
 				"store to JSON");
 		sdb_strbuf_sprintf(errbuf, "Out of memory");
-		sdb_object_deref(SDB_OBJ(f));
+		sdb_object_deref(SDB_OBJ(iter.f));
 		return -1;
 	}
 
-	sdb_store_json_finish(f);
-	sdb_object_deref(SDB_OBJ(f));
+	sdb_store_json_finish(iter.f);
+	sdb_object_deref(SDB_OBJ(iter.f));
 
 	return SDB_CONNECTION_DATA;
 } /* exec_list */
@@ -182,10 +203,10 @@ exec_lookup(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 		int type, sdb_store_matcher_t *m, sdb_store_matcher_t *filter)
 {
 	uint32_t res_type = htonl(SDB_CONNECTION_LOOKUP);
-	sdb_store_json_formatter_t *f;
+	iter_t iter = { NULL, NULL };
 
-	f = sdb_store_json_formatter(buf, type, SDB_WANT_ARRAY);
-	if (! f) {
+	iter.f = sdb_store_json_formatter(buf, type, SDB_WANT_ARRAY);
+	if (! iter.f) {
 		char err[1024];
 		sdb_log(SDB_LOG_ERR, "frontend: Failed to create "
 				"JSON formatter to handle LOOKUP command: %s",
@@ -197,17 +218,17 @@ exec_lookup(sdb_store_t *store, sdb_strbuf_t *buf, sdb_strbuf_t *errbuf,
 
 	sdb_strbuf_memcpy(buf, &res_type, sizeof(uint32_t));
 
-	if (sdb_store_scan(store, type, m, filter, lookup_tojson, f)) {
+	if (sdb_store_scan(store, type, m, filter, lookup_tojson, &iter)) {
 		sdb_log(SDB_LOG_ERR, "frontend: Failed to lookup %ss",
 				SDB_STORE_TYPE_TO_NAME(type));
 		sdb_strbuf_sprintf(errbuf, "Failed to lookup %ss",
 				SDB_STORE_TYPE_TO_NAME(type));
-		sdb_object_deref(SDB_OBJ(f));
+		sdb_object_deref(SDB_OBJ(iter.f));
 		return -1;
 	}
 
-	sdb_store_json_finish(f);
-	sdb_object_deref(SDB_OBJ(f));
+	sdb_store_json_finish(iter.f);
+	sdb_object_deref(SDB_OBJ(iter.f));
 
 	return SDB_CONNECTION_DATA;
 } /* exec_lookup */

t/unit/core/store_json_test.c
@@ -271,54 +271,42 @@ struct {
 	{ { NULL, 0, SDB_DATA_INIT },
 		SDB_SERVICE, scan_tojson_full,
 		"["
-			"{\"name\": \"h2\", \"last_update\": \"1970-01-01 00:00:03 +0000\", "
+			"{\"name\": \"s1\", "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []},"
+			"{\"name\": \"s2\", "
+				"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
 				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"services\": ["
-					"{\"name\": \"s1\", "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []},"
-					"{\"name\": \"s2\", "
+				"\"attributes\": ["
+					"{\"name\": \"k1\", \"value\": 123, "
 						"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": [], "
-						"\"attributes\": ["
-							"{\"name\": \"k1\", \"value\": 123, "
-								"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-								"\"update_interval\": \"0s\", \"backends\": []},"
-							"{\"name\": \"k2\", \"value\": 4711, "
-								"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-								"\"update_interval\": \"0s\", \"backends\": []}"
-						"]}"
+						"\"update_interval\": \"0s\", \"backends\": []},"
+					"{\"name\": \"k2\", \"value\": 4711, "
+						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+						"\"update_interval\": \"0s\", \"backends\": []}"
 				"]}"
 		"]" },
 	{ { NULL, 0, SDB_DATA_INIT },
 		SDB_SERVICE, scan_tojson,
 		"["
-			"{\"name\": \"h2\", \"last_update\": \"1970-01-01 00:00:03 +0000\", "
-				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"services\": ["
-					"{\"name\": \"s1\", "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []},"
-					"{\"name\": \"s2\", "
-						"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []}"
-				"]}"
+			"{\"name\": \"s1\", "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []},"
+			"{\"name\": \"s2\", "
+				"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []}"
 		"]" },
 	{ { sdb_store_gt_matcher, SDB_FIELD_LAST_UPDATE,
 			{ SDB_TYPE_DATETIME, { .datetime = 1 * SDB_INTERVAL_SECOND } } },
 		SDB_SERVICE, scan_tojson_full,
 		"["
-			"{\"name\": \"h2\", \"last_update\": \"1970-01-01 00:00:03 +0000\", "
+			"{\"name\": \"s2\", "
+				"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
 				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"services\": ["
-					"{\"name\": \"s2\", "
+				"\"attributes\": ["
+					"{\"name\": \"k1\", \"value\": 123, "
 						"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": [], "
-						"\"attributes\": ["
-							"{\"name\": \"k1\", \"value\": 123, "
-								"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-								"\"update_interval\": \"0s\", \"backends\": []}"
-						"]}"
+						"\"update_interval\": \"0s\", \"backends\": []}"
 				"]}"
 		"]" },
 	{ { sdb_store_lt_matcher, SDB_FIELD_LAST_UPDATE,
@@ -328,68 +316,48 @@ struct {
 	{ { NULL, 0, SDB_DATA_INIT },
 		SDB_METRIC, scan_tojson_full,
 		"["
-			"{\"name\": \"h1\", \"last_update\": \"1970-01-01 00:00:01 +0000\", "
+			"{\"name\": \"m1\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
 				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"metrics\": ["
-					"{\"name\": \"m1\", "
-						"\"timeseries\": false, "
+				"\"attributes\": ["
+					"{\"name\": \"k3\", \"value\": 42, "
 						"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": [], "
-						"\"attributes\": ["
-							"{\"name\": \"k3\", \"value\": 42, "
-								"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-								"\"update_interval\": \"0s\", \"backends\": []}"
-						"]},"
-					"{\"name\": \"m2\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
 						"\"update_interval\": \"0s\", \"backends\": []}"
 				"]},"
-			"{\"name\": \"h2\", \"last_update\": \"1970-01-01 00:00:03 +0000\", "
-				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"metrics\": ["
-					"{\"name\": \"m1\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []}"
-				"]}"
+			"{\"name\": \"m2\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []},"
+			"{\"name\": \"m1\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []}"
 		"]" },
 	{ { NULL, 0, SDB_DATA_INIT },
 		SDB_METRIC, scan_tojson,
 		"["
-			"{\"name\": \"h1\", \"last_update\": \"1970-01-01 00:00:01 +0000\", "
-				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"metrics\": ["
-					"{\"name\": \"m1\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []},"
-					"{\"name\": \"m2\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []}"
-				"]},"
-			"{\"name\": \"h2\", \"last_update\": \"1970-01-01 00:00:03 +0000\", "
-				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"metrics\": ["
-					"{\"name\": \"m1\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []}"
-				"]}"
+			"{\"name\": \"m1\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:02 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []},"
+			"{\"name\": \"m2\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []},"
+			"{\"name\": \"m1\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []}"
 		"]" },
 	{ { sdb_store_le_matcher, SDB_FIELD_LAST_UPDATE,
 			{ SDB_TYPE_DATETIME, { .datetime = 1 * SDB_INTERVAL_SECOND } } },
 		SDB_METRIC, scan_tojson_full,
 		"["
-			"{\"name\": \"h1\", \"last_update\": \"1970-01-01 00:00:01 +0000\", "
-				"\"update_interval\": \"0s\", \"backends\": [], "
-				"\"metrics\": ["
-					"{\"name\": \"m2\", "
-						"\"timeseries\": false, "
-						"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
-						"\"update_interval\": \"0s\", \"backends\": []}"
-				"]}"
+			"{\"name\": \"m2\", "
+				"\"timeseries\": false, "
+				"\"last_update\": \"1970-01-01 00:00:01 +0000\", "
+				"\"update_interval\": \"0s\", \"backends\": []}"
 		"]" },
 	{ { sdb_store_lt_matcher, SDB_FIELD_LAST_UPDATE,
 			{ SDB_TYPE_DATETIME, { .datetime = 0 } } },

commit 79debed9dacc5d48386124a1ee9a3f7be655fdf6

store_json: Fix parent/child detection.
Rewinding the formatter state has to happen any time we see a parent object of
the current object. This happens every time the current context isn't HOST
(hosts don't have any parents) or the new object is an ATTRIBUTE (which are
always children of whatever other object type).

This broke in 3603db13105aefac4d6ceb356bcfd118a92fa654.

src/core/store_json.c
@@ -142,11 +142,12 @@ handle_new_object(sdb_store_json_formatter_t *f, sdb_store_obj_t *obj)
 		return 0;
 	}
 
-	if ((f->current >= 1) && (obj->type != SDB_ATTRIBUTE)) {
-		/* new entry of a previous type or a new type on the same level;
+	if ((f->context[f->current] != SDB_HOST)
+			&& (obj->type != SDB_ATTRIBUTE)) {
+		/* new entry of the same type or a parent object;
 		 * rewind to the right state */
 		while ((f->current > 0)
-				&& (f->context[f->current] == obj->type)) {
+				&& (f->context[f->current] != obj->type)) {
 			sdb_strbuf_append(f->buf, "}]");
 			--f->current;
 		}